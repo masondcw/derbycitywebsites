553e9cb3af78dd575e8e99d4a886862c0ae8a0a5
masondcw <mason@derbycitywebsites.com>
2024-06-05 17:24:35 -0400

    Add files via upload

--- scroll.js
+++ scroll.js
@@ -11,3 +11,13 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
