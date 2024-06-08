2d7a2069472710b4663d2cef69aa3e96b060a8f3
masondcw <mason@derbycitywebsites.com>
2024-06-08 16:11:41 -0400

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,8 +1,8 @@
-
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if(entry.isIntersecting){
         entry.target.classList.add('show');
+      }
     });
 });
 
